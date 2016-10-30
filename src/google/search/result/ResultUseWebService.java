2390fee772fbcc3291470aeaae7de141a2a315ca
thamluong <thamluong.2ndl@gmail.com>
2016-10-30 13:32:16 +0700

    the 2 result files is almost equivalent with each other in the 10 first results

--- src/google/search/result/ResultUseWebService.java
+++ src/google/search/result/ResultUseWebService.java
@@ -9,5 +9,8 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class ResultUseWebService {
-	
+	/*	now, if we use Web service, 
+	 * the output result is such as the View page source function in browser, not the inspect function
+	 * it's mean that it is the javascript files, not the HTML files
+	 */
 }
